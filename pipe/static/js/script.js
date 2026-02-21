b85746884b0debbe0026f5164dd6547dda7094d7
LascarPaul <23523872+LascarPaul@users.noreply.github.com>
2026-02-21 15:40:49 +0200

    add initial about

--- pipe/static/js/script.js
+++ pipe/static/js/script.js
@@ -1,31 +1,26 @@
 function toggleSections(idDeDeschis) {
-    const sectiuni = document.querySelectorAll(".all-sections");
+  const sectiuni = document.querySelectorAll(".all-sections");
 
-    sectiuni.forEach(div => {
-        div.style.display = 'none';
-    });
+  sectiuni.forEach((div) => {
+    div.style.display = "none";
+  });
 
-    const sectiuneaActiva = document.getElementById(idDeDeschis);
-    if(sectiuneaActiva){
-        sectiuneaActiva.style.display = 'block';
-    }
+  const sectiuneaActiva = document.getElementById(idDeDeschis);
+  if (sectiuneaActiva) {
+    sectiuneaActiva.style.display = "block";
+  }
 
-    if (idDeDeschis === 'gallery-section') {
-        $("#my-gallery").nanogallery2('resize');
-    }
+  if (idDeDeschis === "gallery-section") {
+    $("#my-gallery").nanogallery2("resize");
+  }
 }
 
 function toggleFilters() {
-    let x = document.getElementById("filter-bar");
+  let x = document.getElementById("filter-bar");
 
-    if (x.style.display === "none"){
-        x.style.display = "block";
-    }else {
-        x.style.display = "none";
-    }
-
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
-
-
-
-
